73718c53426a6aa1cb7fa7189b990784ff3989d3
rayane.x.nobrega <rayane.x.nobrega@accenture.com>
2021-03-18 11:46:02 -0300

    storyboard removal

--- BootcampMagic/SceneDelegate.swift
+++ BootcampMagic/SceneDelegate.swift
@@ -8,13 +8,16 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
 
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+                window.rootViewController = ProjectTabBarController()
+                self.window = window
+                window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
