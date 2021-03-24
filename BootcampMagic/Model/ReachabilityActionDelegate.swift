581c033f51ba8d95d856c29ab6dcf7202fb957e7
rayane.x.nobrega <rayane.x.nobrega@accenture.com>
2021-03-24 16:57:33 -0300

    custom alert design

--- BootcampMagic/Model/ReachabilityActionDelegate.swift
+++ BootcampMagic/Model/ReachabilityActionDelegate.swift
@@ -8,9 +8,7 @@
 import Foundation
 import Reachability
 
-//Reachability
-//declare this property where it won't go out of scope relative to your listener
-fileprivate var reachability: Reachability!
+private var reachability: Reachability!
 
 protocol ReachabilityActionDelegate {
     func reachabilityChanged(_ isReachable: Bool)
@@ -21,25 +19,21 @@ protocol ReachabilityObserverDelegate: class, ReachabilityActionDelegate {
     func removeReachabilityObserver()
 }
 
-// Declaring default implementation of adding/removing observer
 extension ReachabilityObserverDelegate {
-    
-    /** Subscribe on reachability changing */
     func addReachabilityObserver() throws {
         reachability = try Reachability()
-        
-        reachability.whenReachable = { [weak self] reachability in
+
+        reachability.whenReachable = { [weak self] _ in
             self?.reachabilityChanged(true)
         }
-        
-        reachability.whenUnreachable = { [weak self] reachability in
+
+        reachability.whenUnreachable = { [weak self] _ in
             self?.reachabilityChanged(false)
         }
-        
+
         try reachability.startNotifier()
     }
-    
-    /** Unsubscribe */
+
     func removeReachabilityObserver() {
         reachability.stopNotifier()
         reachability = nil
